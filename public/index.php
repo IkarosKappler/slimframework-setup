f2d1bfc0df470bcead8dc3c3eb39f8bded190579
ikaros.kappler <root@func.name>
2017-07-24 12:39:28 +0200

    Added Monolog logging.

--- public/index.php
+++ public/index.php
@@ -2,22 +2,37 @@
 use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
 
-require '../vendor/autoload.php';
+if (PHP_SAPI == 'cli-server') {
+    // To help the built-in PHP dev server, check if the request was actually for
+    // something which should probably be served as a static file
+    $url  = parse_url($_SERVER['REQUEST_URI']);
+    $file = __DIR__ . $url['path'];
+    if (is_file($file)) {
+        return false;
+    }
+}
 
+require __DIR__ . '/../vendor/autoload.php';
+
+//require '../vendor/autoload.php';
+
+session_start();
 
 // Configuration
-$config['displayErrorDetails'] = true;
+/*$config['displayErrorDetails'] = true;
 $config['addContentLengthHeader'] = false;
 
 $config['db']['host']   = "localhost";
 $config['db']['user']   = "slim_i2b";
 $config['db']['pass']   = "int2byte";
 $config['db']['dbname'] = "slim_i2b";
+*/
 
+$settings = require __DIR__ . '/../src/settings.php';
 
 
 // Create app
-$app = new \Slim\App( [ 'settings' => $config ] );
+$app = new \Slim\App( [ 'settings' => $settings ] );
 // Clear cached documents after 1 second (for development)
 //$app->expires('+1 seconds');
 
@@ -44,6 +59,20 @@ $app->get('/hello/{name}', function ($request, $response, $args) {
                 'name' => $args['name']
     ]);
 })->setName('profile');
+
+
+
+// Set up dependencies
+require __DIR__ . '/../src/dependencies.php';
+
+// Register middleware
+require __DIR__ . '/../src/middleware.php';
+
+// Register routes
+require __DIR__ . '/../src/routes.php';
+
+
+
 
 // Run app
 $app->run();
